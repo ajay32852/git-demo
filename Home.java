cb85bad723a47449416bc52ebd8c7294c4d6d4f6
Ajay Kumar <ajay32852@gmail.com>
2025-01-29 09:45:43 +0530

    Commit Code By Ajay

--- Home.java
+++ Home.java
@@ -2,7 +2,8 @@ public class Home
 {
  public static void main(String[] args)
  {
-   System.out.println("This is Java First Program test now");
-    System.out.println("Thisn Created By ajay test");
+      System.out.println("This is Java First Program test now");
+      System.out.println("Thisn Created By ajay test");
+      System.out.println("Hey Ajay I want Deployed Code Onn the server");
  }
 }
